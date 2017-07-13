34af7b1c5a239821b42b01c512a0db1fa4330aaa
Daniel Prado <danielprado.ad@gmail.com>
2017-07-13 14:13:21 -0300

    docker

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Rename .js to .ts
 ```bash
 find src -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.ts"' {} \;
 ```
+
+Docker Postgress
+```bash
+docker run --name postgres-node -e POSTGRES_PASSWORD=123 POSTGRES_DB=node POSTGRES_USER=node
+```
--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ const express = require('express'),
   userRoutes = require('./routes/user');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use('/api/user', userRoutes);
-
 app.use((err, req, res, next) => {
   if (err.validationError) {
     return res.status(400).json(err.details);
   }
-
   console.error(err);
   res.status(500).send(err.stack);
 });
-
 app.listen(8080, () => console.log('done.'));
