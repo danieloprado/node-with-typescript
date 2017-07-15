366f2cbf19f8e0fc165436db30b02d25fbac1c7b
Daniel Prado <danielprado.ad@gmail.com>
2017-07-15 07:30:00 -0300

    lines

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const express = require('express'),
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
 app.use('/api/user', userRoutes);
+
 app.use((err, req, res, next) => {
   if (err.validationError) {
     return res.status(400).json(err.details);
@@ -14,4 +16,5 @@ app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send(err.stack);
 });
+
 app.listen(8080, () => console.log('done.'));
